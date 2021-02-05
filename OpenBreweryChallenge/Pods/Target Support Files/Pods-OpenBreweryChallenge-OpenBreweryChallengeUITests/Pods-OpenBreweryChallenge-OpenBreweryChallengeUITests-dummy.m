520621d707c5ecd136b45f4bbf5799f686b5aee9
Thiago Nitschke <thiago.nitschke@gmail.com>
2021-02-05 15:27:05 -0300

    Add Pods directory to project

---
+++ OpenBreweryChallenge/Pods/Target Support Files/Pods-OpenBreweryChallenge-OpenBreweryChallengeUITests/Pods-OpenBreweryChallenge-OpenBreweryChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OpenBreweryChallenge_OpenBreweryChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_OpenBreweryChallenge_OpenBreweryChallengeUITests
+@end
---
+++ OpenBreweryChallenge/Pods/Target Support Files/Pods-OpenBreweryChallengeTests/Pods-OpenBreweryChallengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OpenBreweryChallengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_OpenBreweryChallengeTests
+@end
