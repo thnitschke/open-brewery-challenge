bf9d83b00ff672f073942f0a94460c850867c609
Thiago Nitschke <thiago.nitschke@gmail.com>
2021-02-05 15:21:29 -0300

    Start up project files + cocoapods

---
+++ OpenBreweryChallenge/OpenBreweryChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenBreweryChallenge
+//
+//  Created by Thiago Nitschke Simões on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
